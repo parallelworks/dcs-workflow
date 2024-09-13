b8e686d9090681a819bb7ebf426a0a270edc4c17
avidalto <avidalto@hawk.iit.edu>
2024-09-13 09:31:03 -0500

    Simplify metering

--- metering.sh
+++ metering.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while true; do
     sleep 60
-    ssh -A -o StrictHostKeyChecking=no ${resource_publicIp} rsync -avz ${resource_jobdir}/usage/ ${metering_user}@{metering_ip}:~/.3dcs/usage-pending
+    ssh -A -o StrictHostKeyChecking=no ${resource_publicIp} rsync -avz ${resource_jobdir}/usage/ ${metering_user}@${metering_ip}:~/.3dcs/usage-pending
 done
