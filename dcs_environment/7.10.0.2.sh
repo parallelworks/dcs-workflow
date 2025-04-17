a25bffe2235d5501889a46ac6a944aef44835ef7
avidalto <avidalto@hawk.iit.edu>
2025-04-17 10:42:04 -0500

    Support rocky linux 8

--- dcs_environment/7.10.0.2.sh
+++ dcs_environment/7.10.0.2.sh
@@ -1,3 +1,5 @@
-export PATH=/dcs/wine/8.0.2/bin:$PATH
-export WINEPREFIX=/dcs/wine/apps/3dcs/7.10.0.2/.wine
+source /pw/apps/etc/pw-apps.sh 
+module load wine/8.0.2
+export WINEPREFIX=/dcs/wine/apps/3dcs/.wine
+export dcs_run="wine /dcs/wine/apps/3dcs/.wine/drive_c/Program\ Files/DCS/3DCS_MC_8_0_0_2/dcsSimuMacro.exe"export WINEPREFIX=/dcs/wine/apps/3dcs/7.10.0.2/.wine
 export dcs_run="wine /dcs/wine/apps/3dcs/7.10.0.2/.wine/drive_c/Program\ Files/DCS/3DCS_MC_7_10_0_2/dcsSimuMacro.exe "
--- dcs_environment/8.0.0.2.sh
+++ dcs_environment/8.0.0.2.sh
@@ -1,3 +1,4 @@
-export PATH=/dcs/wine/8.0.2/bin:$PATH
+source /pw/apps/etc/pw-apps.sh 
+module load wine/8.0.2
 export WINEPREFIX=/dcs/wine/apps/3dcs/.wine
 export dcs_run="wine /dcs/wine/apps/3dcs/.wine/drive_c/Program\ Files/DCS/3DCS_MC_8_0_0_2/dcsSimuMacro.exe"
