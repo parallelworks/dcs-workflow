b8d2436e22c52ff77583c876b53212b484702de4
Alvaro Vidal Torreira <alvaro@parallelworks.com>
2025-06-18 17:46:35 +0200

    Change load wine command

--- dcs_environment/7.10.0.2.sh
+++ dcs_environment/7.10.0.2.sh
@@ -1,3 +1,4 @@
-export PATH=/dcs/wine/8.0.2/bin:$PATH
+source /pw/apps/etc/pw-apps.sh 
+module load wine/8.0.2
 export WINEPREFIX=/dcs/wine/apps/3dcs/7.10.0.2/.wine
 export dcs_run="wine /dcs/wine/apps/3dcs/7.10.0.2/.wine/drive_c/Program\ Files/DCS/3DCS_MC_7_10_0_2/dcsSimuMacro.exe "
