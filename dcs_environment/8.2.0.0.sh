713843f4d1eea21cdcfaea9e1a7d56daa017119e
Alvaro Vidal Torreira <alvaro@parallelworks.com>
2025-07-11 16:54:40 +0200

    Add support for dcs version 8.2.0.0

---
+++ dcs_environment/8.2.0.0.sh
@@ -0,0 +1,4 @@
+source /pw/apps/etc/pw-apps.sh 
+module load wine/8.0.2
+export WINEPREFIX=/dcs/wine/apps/3dcs/8.2.0.0/.wine
+export dcs_run="wine /dcs/wine/apps/3dcs/8.2.0.0/.wine/drive_c/Program Files/DCS/3DCS_MC_8_2_0_0/dcsSimuMacro.exe"
