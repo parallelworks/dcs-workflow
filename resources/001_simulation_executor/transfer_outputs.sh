8bf0ae820cf09d597b67775d546bb4e2eacda9d8
Alvaro Vidal Torreira <alvaro@parallelworks.com>
2026-02-26 16:47:27 +0100

    Update transfer_outputs.sh (#39)

--- resources/001_simulation_executor/transfer_outputs.sh
+++ resources/001_simulation_executor/transfer_outputs.sh
@@ -1,5 +1,23 @@
 # HARDCODED TO AWS
 # A dynamicStorage parameter type would be very helpful for this
+unset BUCKET_URI
+unset AWS_ACCESS_KEY_ID
+unset AWS_SECRET_ACCESS_KEY
+unset AWS_SESSION_TOKEN
+
+resource_jobdir=/home/Hanif/pw/jobs/marketplace.3dcshondajapan.latest/00041
+source ${resource_jobdir}/bucket_credentials
+
+# Loop until all variables are non-empty
+while [[ -z "$BUCKET_URI" \
+     || -z "$AWS_ACCESS_KEY_ID" \
+     || -z "$AWS_SECRET_ACCESS_KEY" \
+     || -z "$AWS_SESSION_TOKEN" ]]
+do
+    echo "$(date) Waiting for required environment variables to be set..."
+    sleep 5
+    source ${resource_jobdir}/bucket_credentials
+done
 
 # Copy path/to/worker_<i> to bucket
 aws s3 cp --recursive ${PWD} ${BUCKET_URI}/${dcs_output_directory}/${USER}/${workflow_name}/${job_number}/$(basename ${PWD})
