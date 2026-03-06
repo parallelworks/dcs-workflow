efa8386ce7b35908348909d30fa52f07291a200e
Alvaro Vidal Torreira <alvaro@parallelworks.com>
2026-03-06 16:05:25 +0100

    testing

--- scripts/transfer_outputs.sh
+++ scripts/transfer_outputs.sh
@@ -1,5 +1,6 @@
 
 # Copy path/to/worker_<i> to bucket
-pw buckets cp -r ${PWD} ${dcs_bucket_uri}/${dcs_output_directory}/${USER}/${PW_WORKFLOW_NAME}/${PW_JOB_NUMBER}/$(basename ${PWD})
+set -x
+pw buckets cp -r ${PWD} ${dcs_bucket_uri}/${dcs_output_directory}/${USER}/${PW_WORKFLOW_NAME}/${PW_JOB_NUMBER}/$(basename ${PWD}) > bucket_upload.log 2>&1
 
 
