38a90ec161f1cfb1df69da2e237451434cc34516
Alvaro Vidal Torreira <alvaro@parallelworks.com>
2026-03-09 14:35:35 +0100

    Add usage metering

--- metering.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir -p usage
-while true; do
-    sleep 60
-    rsync -avz --delete ${resource_publicIp}:${resource_jobdir}/usage/ usage >> metering.out 2>&1
-    rsync -avz ${resource_jobdir}/usage/ ${metering_user}@${metering_ip}:~/.3dcs/usage-pending >> metering.out 2>&1
-done
